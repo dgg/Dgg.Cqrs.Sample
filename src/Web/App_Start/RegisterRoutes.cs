8228b6ba1d3bc67ab387c2574251c067c1513b22
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2011-11-01 23:21:34 +0100

    use web activator for bootstrapping instead of changing global.asax

---
+++ src/Web/App_Start/RegisterRoutes.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Dgg.Cqrs.Sample.Core.Infrastructure.Bootstrapping;
+
+[assembly: WebActivator.PostApplicationStartMethod(typeof(Dgg.Cqrs.Sample.Web.App_Start.Bootstrapper), "PostStart")]
+
+namespace Dgg.Cqrs.Sample.Web.App_Start
+{
+	public static class Bootstrapper
+	{
+		public static void PostStart()
+		{
+			new ApplicationBootstrapper()
+				.RegisterAreas()
+				.RegisterRoutes(RouteTable.Routes)
+				.RegisterGlobalFilters(GlobalFilters.Filters)
+				.ConfigureIoC();
+		}
+	}
+}
--- src/Web/Global.asax.cs
+++ src/Web/Global.asax.cs
@@ -1,19 +1,9 @@
 ﻿using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-using Dgg.Cqrs.Sample.Core.Infrastructure.Bootstrapping;
 
 namespace Dgg.Cqrs.Sample.Web
 {
 	public class MvcApplication : HttpApplication
 	{
-		protected void Application_Start()
-		{
-			new ApplicationBootstrapper()
-				.RegisterAreas()
-				.RegisterRoutes(RouteTable.Routes)
-				.RegisterGlobalFilters(GlobalFilters.Filters)
-				.ConfigureIoC();
-		}
+		protected void Application_Start() { }
 	}
 }
