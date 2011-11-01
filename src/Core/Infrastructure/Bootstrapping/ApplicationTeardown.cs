7d1976c1e85ef9ff3e29ca1c6ebf2962acda02a5
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2011-11-01 23:38:14 +0100

    moved away from WebActivator to include RequestEnd handler and installed ravendb

---
+++ src/Core/Infrastructure/Bootstrapping/ApplicationTeardown.cs
@@ -0,0 +1,13 @@
+﻿using StructureMap;
+
+namespace Dgg.Cqrs.Sample.Core.Infrastructure.Bootstrapping
+{
+	public class ApplicationTeardown
+	{
+		public ApplicationTeardown WhenEndingRequest()
+		{
+			ObjectFactory.ReleaseAndDisposeAllHttpScopedObjects();
+			return this;
+		}
+	}
+}
--- src/Web/Global.asax.cs
+++ src/Web/Global.asax.cs
@@ -1,9 +1,25 @@
 ﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Dgg.Cqrs.Sample.Core.Infrastructure.Bootstrapping;
 
 namespace Dgg.Cqrs.Sample.Web
 {
 	public class MvcApplication : HttpApplication
 	{
-		protected void Application_Start() { }
+		protected void Application_Start()
+		{
+			// does not use WebActivator as we still need to wire to the EndRequest
+			new ApplicationBootstrapper()
+				.RegisterAreas()
+				.RegisterRoutes(RouteTable.Routes)
+				.RegisterGlobalFilters(GlobalFilters.Filters)
+				.ConfigureIoC();
+		}
+
+		protected void Application_EndRequest()
+		{
+			new ApplicationTeardown().WhenEndingRequest();
+		}
 	}
 }
